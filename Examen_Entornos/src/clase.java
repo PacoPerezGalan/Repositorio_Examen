4cb36dc63b5cc4555dd53509603d78e17e5a51b0
1dam <1dam@WINMAC2.alumnado.flo>
2016-05-23 15:43:43 +0200

    toma toma

--- Examen_Entornos/src/clase.java
+++ Examen_Entornos/src/clase.java
@@ -1,4 +1,10 @@
-
+ 
 public class clase {
-	for(int i=0;i<10000;i++) System.out.println("Guillermo me molas mucho");
+	
+	public clase(){
+		for(int i=0;i<10000;i++){ 
+			System.out.println("Guillermo me molas mucho");
+	
+		}
+	}
 }
